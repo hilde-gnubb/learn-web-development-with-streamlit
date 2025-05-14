4aea83cfe431a9d4e83974e27daf9e5d0e95b2ac
hilde-gnubb <hilde.gnubb@gmx.de>
2025-05-14 14:07:57 +0000

    erste App

--- app/main.py
+++ app/main.py
@@ -1,5 +1,25 @@
 import streamlit as st
 from links import FROH, TRAURIG
 
-# Emojis: 😎 💻 🚀 😢 😁
-# mehr Emojis gibt es hier: https://apps.timwhitlock.info/emoji/tables/unicode
+st.title("Hallo👋")
+
+st.markdown(
+    """ 
+     
+
+    **zu unserem :rainbow[Zufallsgenerator]!**
+    
+    
+    """
+)
+
+zeige_ballons = st.button("Send balloons!")
+if zeige_ballons:
+   st.balloons()
+
+alle_namen = []
+neuer_name = st.text_input("Bitte gebe einen Namen ein")
+alle_namen.append(neuer_name)
+st.write(neuer_name)
+users = st.multiselect("Personen", alle_namen)
+rolling_average = st.toggle("Rolling average")
