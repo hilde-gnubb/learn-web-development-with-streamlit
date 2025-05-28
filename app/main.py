ac02e3bf4c6f074b66bfa399670be19cbda1569f
hilde-gnubb <hilde.gnubb@gmx.de>
2025-05-28 14:04:35 +0000

    zg1

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from links import FROH, TRAURIG
-
+import numpy
+import random
 state = st.session_state
 if "alle_namen" not in state:
    state["alle_namen"] = []
@@ -18,8 +19,8 @@ zeige_ballons = st.button("Send balloons!")
 if zeige_ballons:
    st.balloons()
 
-neuer_name = st.text_input("Bitte gebe einen Namen ein", value=None)
-if neuer_name is not None and neuer_name not in state["alle_namen"]:
+neuer_name = st.text_input("Bitte gebe einen Namen ein", value="")
+if neuer_name != "" and neuer_name not in state["alle_namen"]:
     state["alle_namen"].append(neuer_name)
 
 st.write(state["alle_namen"])
@@ -30,5 +31,20 @@ import streamlit as st
 
 option = st.selectbox(
     "Wie viele Personen sollen in einer Gruppe sein",
-    ("2", "3", "4", "5", "6"),
+    (2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15),
 )
+personen_auswahl = st.button("Gruppen")
+if personen_auswahl:
+   #persons = numpy.random.choice(state["alle_namen"],size = option,replace = False)
+   #st.write(persons)
+   mögliche_personen = list(state["alle_namen"])
+   gruppen = []
+   nächste_gruppe = []
+   while len(mögliche_personen) > 0:
+      person = random.choice(mögliche_personen)
+      mögliche_personen.remove(person)
+      nächste_gruppe.append(person)
+      if len(nächste_gruppe) == option:
+         gruppen.append(nächste_gruppe)
+         nächste_gruppe = []
+   st.write(gruppen)
